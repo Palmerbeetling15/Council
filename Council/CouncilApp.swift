a5f53e2ee2b430c198f50fa383b554a7bb2d646b
Joseph <joseph.thecouncil@gmail.com>
2026-05-28 03:57:21 +0300

    Initial Commit

---
+++ Council/CouncilApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouncilApp.swift
+//  Council
+//
+//  Created by Sina on 28.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct CouncilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
